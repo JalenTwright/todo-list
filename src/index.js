20a917266282d65f0dd3502f0ec2d1c6108501f0
jalentwright <jalen_t_wright@yahoo.com>
2024-05-27 00:56:34 -0500

    added some notes for how to deal with project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const body = document.querySelector('body');
-
+//just to check and see if file worked
 function content() {
     const hello = document.createElement("div");
     hello.classList.add("title");
     hello.innerHTML = "Hello";
 
     body.appendChild(hello);
-    
+    //just test
     return {
         hello
     }
 }
-
 content();
+
+//code for project tabs first priority
+    //maybe use a for each loop for every project tab and module
 
